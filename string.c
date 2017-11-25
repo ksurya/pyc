340d9062cb8b8d04f2f52a503ae54c5b2465ff1f
Surya Kasturi <kasturisurya@gmail.com>
2017-11-24 20:26:35 -0500

    swapcase, ljust, rjust, count functions

--- string.c
+++ string.c
@@ -6,18 +6,29 @@
 #include <string.h>
 
 
-// globals
-const char _ASCII_LOWER_MIN = 'a';
-const char _ASCII_LOWER_MAX = 'z';
-const char _ASCII_UPPER_MIN = 'A';
-const char _ASCII_UPPER_MAX = 'Z';
-
-
 char *capitalize(char *new, const char *line) {
     strcpy(new, line);
-    if (*new >= _ASCII_LOWER_MIN && *new <= _ASCII_LOWER_MAX)
-        *new = *new - _ASCII_LOWER_MIN + _ASCII_UPPER_MIN;
+    if (*new >= 'a' && *new <= 'z')
+        *new = 'A' + *new - 'a';
     return new;
+}
+
+
+char *swapcase(char *new, const char *line) {
+    char *start = new;
+
+    while (*line) {
+        if (*line >= 'A' &&  *line <= 'Z')
+            *new = 'a' + *line - 'A';
+        else if (*line >= 'a' && *line <= 'z')
+            *new = 'A' + *line - 'a';
+        else
+            *new = *line;
+        new++;
+        line++;
+    }
+
+    return start;
 }
 
 
@@ -72,4 +83,52 @@ int rfind(const char *str, const char *sub) {
             return pre;
 
     return -1;
+}
+
+
+int scount(const char *str, const char *sub) {
+    int sub_len = strlen(sub);
+    int count = 0;
+    int pre;
+
+    for (pre = 0; pre < strlen(str); pre += sub_len)
+        count += startswith(str + pre, sub);
+
+    return count;
+}
+
+
+char *ljust(char *jst, const char *str, int width, char fill) {
+    char *start = jst;
+    int len = strlen(str);
+    int cnt;
+
+    if (len >= width) {
+        while ((*jst++ = *str++) != '\0');
+        return start;
+    }
+
+    for (cnt = 0; cnt < width - len; cnt++)
+        *(jst + cnt) = fill;
+
+    while ((*(jst++ + cnt) = *str++) != '\0');
+
+    return start;
+}
+
+
+char *rjust(char *jst, const char *str, int width, char fill) {
+    char *start = jst;
+    int len = strlen(str);
+    int cnt = 0;
+
+    while ((*jst++ = *str++) != '\0');
+
+    if (len >= width)
+        return start;
+
+    for (jst--; cnt < width - len; cnt++, *++jst = 0)
+        *jst = fill;
+
+    return start;
 }
