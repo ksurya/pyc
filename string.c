27d20710799621eb71ee8863b47654ec83586fb1
Surya Kasturi <kasturisurya@gmail.com>
2017-11-26 02:14:19 -0500

    Few more functions

--- string.c
+++ string.c
@@ -1,13 +1,24 @@
 //
-// Python string library implementation in C
+// Python string methods implementation in C
 //
 
-#include <stdio.h>
-#include <string.h>
+
+char *strcopy(char *new, const char *str) {
+    char *start = new;
+    while ((*new++ = *str++) != '\0');
+    return start;
+}
+
+
+int strlength(const char *s) {
+    int i = 0;
+    while (*s++ != '\0') i++;
+    return i;
+}
 
 
 char *capitalize(char *new, const char *line) {
-    strcpy(new, line);
+    strcopy(new, line);
     if (*new >= 'a' && *new <= 'z')
         *new = 'A' + *new - 'a';
     return new;
@@ -54,18 +65,33 @@ int startswith(const char *str, const char *sub) {
     if (!*sub || !*str)
         return 0;
 
-    while (*sub)
+    while (*sub && *str)
         if (*str++ != *sub++)
             return 0;
-    
-    return 1;
+
+    return !*sub;
+}
+
+
+int endswith(const char *str, const char *sub) {
+    int i = strlength(str);
+    int j = strlength(sub);
+
+    if (!*sub || !*str)
+        return 0;
+
+    while (i >= 0 && j >= 0)
+        if (*(str + i--) != *(sub + j--))
+            return 0;
+
+    return j < 0;
 }
 
 
 int find(const char *str, const char *sub) {
     int pre;
 
-    for (pre = 0; pre < strlen(str); pre++)
+    for (pre = 0; pre < strlength(str); pre++)
         if (startswith(str + pre, sub))
             return pre;
 
@@ -74,8 +100,8 @@ int find(const char *str, const char *sub) {
 
 
 int rfind(const char *str, const char *sub) {
-    int str_len = strlen(str);
-    int sub_len = strlen(sub);
+    int str_len = strlength(str);
+    int sub_len = strlength(sub);
     int pre;
 
     for (pre = str_len - sub_len; pre >= 0; pre--)
@@ -87,11 +113,11 @@ int rfind(const char *str, const char *sub) {
 
 
 int scount(const char *str, const char *sub) {
-    int sub_len = strlen(sub);
+    int sub_len = strlength(sub);
     int count = 0;
     int pre;
 
-    for (pre = 0; pre < strlen(str); pre += sub_len)
+    for (pre = 0; pre < strlength(str); pre += sub_len)
         count += startswith(str + pre, sub);
 
     return count;
@@ -100,7 +126,7 @@ int scount(const char *str, const char *sub) {
 
 char *ljust(char *jst, const char *str, int width, char fill) {
     char *start = jst;
-    int len = strlen(str);
+    int len = strlength(str);
     int cnt;
 
     if (len >= width) {
@@ -119,7 +145,7 @@ char *ljust(char *jst, const char *str, int width, char fill) {
 
 char *rjust(char *jst, const char *str, int width, char fill) {
     char *start = jst;
-    int len = strlen(str);
+    int len = strlength(str);
     int cnt = 0;
 
     while ((*jst++ = *str++) != '\0');
@@ -145,8 +171,8 @@ char *replace(char *new, const char *str, const char *old_sub,
     int i = 0;
     int pos = 0;
     char *start = new;
-    int new_sub_len = strlen(new_sub);
-    int old_sub_len = strlen(old_sub);
+    int new_sub_len = strlength(new_sub);
+    int old_sub_len = strlength(old_sub);
     
     while (max_replace-- && (pos = find(str, old_sub)) >= 0) {        
         for (i = 0; i < pos; i++)
@@ -177,7 +203,7 @@ char *lstrip(char *new, const char *str) {
 
 char *rstrip(char *new, const char *str) {
     char *start = new;
-    const char *end = str + strlen(str) - 1;
+    const char *end = str + strlength(str) - 1;
 
     while (end >= str) {
         if (*end != '\t' && *end != '\r' && *end != '\n' && *end != ' ')
@@ -210,4 +236,72 @@ char *join(char *new, char **str, char sep) {
     }
 
     return start;
+}
+
+
+int _isalpha(const char *s) {
+    if (!*s)
+        return 0;
+    while (*s) {
+        if (!(*s >= 'A' && *s <= 'Z') && !(*s >= 'a' && *s <= 'z'))
+            return 0;
+        s++;
+    }
+    return 1;
+}
+
+
+int _isdigit(const char *s) {
+    if (!*s)
+        return 0;
+    while (*s) {
+        if (!(*s >= '0' && *s <= '9'))
+            return 0;
+        s++;
+    }
+    return 1;
+}
+
+
+int _isalnum(const char *s) {
+    if (!*s)
+        return 0;
+    while (*s) {
+        if (!(*s >= '0' && *s <= '9') && 
+            !(*s >= 'A' && *s <= 'Z') && 
+            !(*s >= 'a' && *s <= 'z'))
+            return 0;
+        s++;
+    }
+    return 1;
+}
+
+
+int _islower(const char *s) {
+    while (*s) {
+        if (*s >= 'A' && *s <= 'Z')
+            return 0;
+        s++;
+    }
+    return 1;
+}
+
+
+int _isupper(const char *s) {
+    while (*s) {
+        if (*s >= 'a' && *s <= 'z')
+            return 0;
+        s++;
+    }
+    return 1;
+}
+
+
+int _isspace(const char *s) {
+    while (*s) {
+        if (*s != '\t' || *s != '\n' || *s != '\r' || *s != ' ')
+            return 0;
+        s++;
+    }
+    return 1;
 }
